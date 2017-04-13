3d5f5638c115875ba0969729c1ff43fe8167b5d7
Ryan Spink <ryanspinktech@gmail.com>
2017-04-13 11:48:01 -0600

    working pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @stores = Store.search_best_buy(params[:search])
+    @stores = Store.search_best_buy(params[:search]).paginate(:page => params[:page], :per_page => 10)
   end
 end
