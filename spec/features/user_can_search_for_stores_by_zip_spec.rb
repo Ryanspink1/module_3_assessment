00e8e892e77224bfa2da55eb603b4137a5f92b73
Ryan Spink <ryanspinktech@gmail.com>
2017-04-13 11:03:49 -0600

    feature testing complete

--- spec/features/user_can_search_for_stores_by_zip_spec.rb
+++ spec/features/user_can_search_for_stores_by_zip_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
-  describe 'when a user searches for stores by zip code'
-    it 'they are given a list of stores within 25 miles of that zip code'
+  describe 'when a user searches for stores by zip code' do
+    it 'they are given a list of stores within 25 miles of that zip code' do
 
-    visit ''
+      visit '/items'
+
+
+      fill_in "search", with: "80202"
+      click_button 'Search'
+
+
+      expect(current_path).to eq('/search')
+
+      expect(page).to have_content("CHERRY CREEK MALL")
+      expect(page).to have_content("DENVER")
+      expect(page).to have_content("3.45")
+      expect(page).to have_content("Mobile SAS")
+
+      expect(page).to_not have_content("Cleveland Mall")
+      expect(page).to_not have_content("Cleveland")
+      expect(page).to_not have_content("200000")
+      expect(page).to_not have_content("Jabron")
+    end
+  end
